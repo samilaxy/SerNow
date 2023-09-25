234d1b2284fb28648e0abfd848625dd6e7b4bfcf
samilaxy <samuelnoye7@gmail.com>
2023-09-25 17:30:45 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ void main() async {
 
   bool userLoggedIn = await authProvider.loginState();
 
-  String initialRoute = userLoggedIn ? 'home' : 'home';
+  String initialRoute = userLoggedIn ? 'home' : 'phone';
 
   runApp(
     MultiProvider(
