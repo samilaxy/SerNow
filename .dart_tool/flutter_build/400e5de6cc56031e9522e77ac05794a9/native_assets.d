02ff9ef15520cf93c5c7357fb0e5cf3c049fbc87
Noye Samuel <samuel.noye@amalitech.org>
2023-10-25 16:41:51 +0000

    stable version

---
+++ .dart_tool/flutter_build/400e5de6cc56031e9522e77ac05794a9/native_assets.d
@@ -0,0 +1,1 @@
+ /Users/amalitech-pc-4100433/Documents/Projects/ServNow/.dart_tool/flutter_build/400e5de6cc56031e9522e77ac05794a9/native_assets.yaml: 
---
+++ .dart_tool/flutter_build/bc3d2bde0dfc48fa0c85695247696cad/native_assets.d
@@ -0,0 +1,1 @@
+ /Users/amalitech-pc-4100433/Documents/Projects/ServNow/.dart_tool/flutter_build/bc3d2bde0dfc48fa0c85695247696cad/native_assets.yaml: 
