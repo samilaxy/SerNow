a43f7fb984cca903f0a783a7770b07f92db55d4a
Samnoye <samuelnoye7@gmail.com>
2024-07-23 10:16:02 +0000

    updated packages

--- build/ios/Debug-iphonesimulator/cloud_firestore/cloud_firestore.framework/Headers/FirestoreMessages.g.h
+++ build/ios/Debug-iphonesimulator/cloud_firestore/cloud_firestore.framework/Headers/FirestoreMessages.g.h
@@ -57,6 +57,29 @@ typedef NS_ENUM(NSUInteger, Source) {
 - (instancetype)initWithValue:(Source)value;
 @end
 
+/// The listener retrieves data and listens to updates from the local Firestore cache only.
+/// If the cache is empty, an empty snapshot will be returned.
+/// Snapshot events will be triggered on cache updates, like local mutations or load bundles.
+///
+/// Note that the data might be stale if the cache hasn't synchronized with recent server-side
+/// changes.
+typedef NS_ENUM(NSUInteger, ListenSource) {
+  /// The default behavior. The listener attempts to return initial snapshot from cache and retrieve
+  /// up-to-date snapshots from the Firestore server.
+  /// Snapshot events will be triggered on local mutations and server side updates.
+  ListenSourceDefaultSource = 0,
+  /// The listener retrieves data and listens to updates from the local Firestore cache only.
+  /// If the cache is empty, an empty snapshot will be returned.
+  /// Snapshot events will be triggered on cache updates, like local mutations or load bundles.
+  ListenSourceCache = 1,
+};
+
+/// Wrapper for ListenSource to allow for nullability.
+@interface ListenSourceBox : NSObject
+@property(nonatomic, assign) ListenSource value;
+- (instancetype)initWithValue:(ListenSource)value;
+@end
+
 typedef NS_ENUM(NSUInteger, ServerTimestampBehavior) {
   /// Return null for [FieldValue.serverTimestamp()] values that have not yet
   ServerTimestampBehaviorNone = 0,
@@ -110,6 +133,18 @@ typedef NS_ENUM(NSUInteger, PigeonTransactionType) {
 - (instancetype)initWithValue:(PigeonTransactionType)value;
 @end
 
+typedef NS_ENUM(NSUInteger, AggregateType) {
+  AggregateTypeCount = 0,
+  AggregateTypeSum = 1,
+  AggregateTypeAverage = 2,
+};
+
+/// Wrapper for AggregateType to allow for nullability.
+@interface AggregateTypeBox : NSObject
+@property(nonatomic, assign) AggregateType value;
+- (instancetype)initWithValue:(AggregateType)value;
+@end
+
 @class PigeonFirebaseSettings;
 @class FirestorePigeonFirebaseApp;
 @class PigeonSnapshotMetadata;
@@ -121,6 +156,8 @@ typedef NS_ENUM(NSUInteger, PigeonTransactionType) {
 @class PigeonTransactionCommand;
 @class DocumentReferenceRequest;
 @class PigeonQueryParameters;
+@class AggregateQuery;
+@class AggregateQueryResponse;
 
 @interface PigeonFirebaseSettings : NSObject
 /// `init` unavailable to enforce nonnull fields, see the `make` class method.
@@ -257,6 +294,25 @@ typedef NS_ENUM(NSUInteger, PigeonTransactionType) {
 @property(nonatomic, strong, nullable) NSDictionary<NSString *, id> *filters;
 @end
 
+@interface AggregateQuery : NSObject
+/// `init` unavailable to enforce nonnull fields, see the `make` class method.
+- (instancetype)init NS_UNAVAILABLE;
++ (instancetype)makeWithType:(AggregateType)type field:(nullable NSString *)field;
+@property(nonatomic, assign) AggregateType type;
+@property(nonatomic, copy, nullable) NSString *field;
+@end
+
+@interface AggregateQueryResponse : NSObject
+/// `init` unavailable to enforce nonnull fields, see the `make` class method.
+- (instancetype)init NS_UNAVAILABLE;
++ (instancetype)makeWithType:(AggregateType)type
+                       field:(nullable NSString *)field
+                       value:(nullable NSNumber *)value;
+@property(nonatomic, assign) AggregateType type;
+@property(nonatomic, copy, nullable) NSString *field;
+@property(nonatomic, strong, nullable) NSNumber *value;
+@end
+
 /// The codec used by FirebaseFirestoreHostApi.
 NSObject<FlutterMessageCodec> *FirebaseFirestoreHostApiGetCodec(void);
 
@@ -318,12 +374,14 @@ NSObject<FlutterMessageCodec> *FirebaseFirestoreHostApiGetCodec(void);
            parameters:(PigeonQueryParameters *)parameters
               options:(PigeonGetOptions *)options
            completion:(void (^)(PigeonQuerySnapshot *_Nullable, FlutterError *_Nullable))completion;
-- (void)aggregateQueryCountApp:(FirestorePigeonFirebaseApp *)app
-                          path:(NSString *)path
-                    parameters:(PigeonQueryParameters *)parameters
-                        source:(AggregateSource)source
-             isCollectionGroup:(NSNumber *)isCollectionGroup
-                    completion:(void (^)(NSNumber *_Nullable, FlutterError *_Nullable))completion;
+- (void)aggregateQueryApp:(FirestorePigeonFirebaseApp *)app
+                     path:(NSString *)path
+               parameters:(PigeonQueryParameters *)parameters
+                   source:(AggregateSource)source
+                  queries:(NSArray<AggregateQuery *> *)queries
+        isCollectionGroup:(NSNumber *)isCollectionGroup
+               completion:(void (^)(NSArray<AggregateQueryResponse *> *_Nullable,
+                                    FlutterError *_Nullable))completion;
 - (void)writeBatchCommitApp:(FirestorePigeonFirebaseApp *)app
                      writes:(NSArray<PigeonTransactionCommand *> *)writes
                  completion:(void (^)(FlutterError *_Nullable))completion;
@@ -333,10 +391,12 @@ NSObject<FlutterMessageCodec> *FirebaseFirestoreHostApiGetCodec(void);
                 parameters:(PigeonQueryParameters *)parameters
                    options:(PigeonGetOptions *)options
     includeMetadataChanges:(NSNumber *)includeMetadataChanges
+                    source:(ListenSource)source
                 completion:(void (^)(NSString *_Nullable, FlutterError *_Nullable))completion;
 - (void)documentReferenceSnapshotApp:(FirestorePigeonFirebaseApp *)app
                           parameters:(DocumentReferenceRequest *)parameters
               includeMetadataChanges:(NSNumber *)includeMetadataChanges
+                              source:(ListenSource)source
                           completion:
                               (void (^)(NSString *_Nullable, FlutterError *_Nullable))completion;
 @end
